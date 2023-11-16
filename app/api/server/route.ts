00d8b2cdc9b2200621c758338c127f2439f60ae7
steve-ababio <stevo12424@live.com>
2023-11-16 19:51:30 +0000

    deleted signin page, deleted signup page and route.tsfiles

--- app/api/server/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse,NextRequest } from "next/server";
-import { Server } from "socket.io";
-
-function GET(req:NextRequest,res:Response){
-    const io = new Server();
-    const socket = io.on("connection",function)
-}
